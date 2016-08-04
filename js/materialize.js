36ee646e6445a0bc6b5317c9fcb4d781b8b61ca6
Peter Guerrero <geterp@hotmail.com>
2016-08-04 12:23:38 -0500

    first push

---
+++ js/materialize.js
@@ -0,0 +1,125 @@
+// $(document).ready(function(){
+// 			$('select').material_select();
+// 			// $('.modal-trigger').leanModal();
+	
+// 		});		
+// 		//---MODAL----
+//   			$('#open-modal').on("click", function(){
+//    				$('#modal1').openModal();
+//    				$("body").append(overlay);		
+//   			});
+  		 
+
+// --------------------------------------------------------------------------------------------------
+
+
+	$(document).ready(function(){
+    			// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    			// $('.modal-trigger').leanModal();
+				  			
+				// ---MODAL----
+  				$('#open-model').on("click", function(){
+   					$('#modal1').openModal();
+
+  				});
+  			});
+
+$(document).ready(function(){
+    			// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    			// $('.modal-trigger').leanModal();
+				  			
+				// ---MODAL----
+  				$('#open-model2').on("click", function(){
+   					$('#modal2').openModal();
+   					
+  				});
+  			});
+
+
+
+$(document).ready(function(){
+    			// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    			// $('.modal-trigger').leanModal();
+				  			
+				// ---MODAL----
+  				$('#open-model3').on("click", function(){
+   					$('#modal3').openModal();
+   					
+  				});
+  			});
+
+// --------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+  		//  $(document).ready(function(){
+    // 			// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    // 			$('.modal-trigger').leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+				  			
+				// // ---MODAL----
+  		// 		$('#open-modal').on("click", function(){
+   	// 				$('#modal1').openModal();
+  		// 		});
+  		// 	});
+
+
+
+// --------------------------------------------------------------------------------------------------
+
+
+  	// 	 (document).ready(function(){
+			// $('select').material_select();
+			
+			// //---MODAL----
+		 //  	$('#open-model').on("click", function(){
+		 //   		$('#modal1').openModal();
+		 //  	});
+		 //  });
+
+// --------------------------------------------------------------------------------------------------
+
+
+
+
+	 // $(document).ready(function(){
+  //           $('.modal-trigger').click(function() {
+  //                $('#modal1').openModal();
+  //           });
+  //        });   
+  //           if ($('body').css(''))
+  //           {
+  //               $('.lean-overlay').remove();
+  //           };
+
+
+
+
+            // this is to deal with dark overlay issue but does not work 
+    //         $('#open-modal').openModal({
+    // 		 dismissible: dismissible
+				// });
+
+				// // grab the dark overlay
+				// var overlay = $('.lean-overlay');
+				// // remove it
+				// overlay.detach();
+				// // attach it to the thing you want darkened
+				// $('.container').after(overlay);
+
+
+				// // close the modal
+				// $(modal).closeModal();
+				// // grab the dark overlay
+				// var overlay = $('.lean-overlay');
+				// // remove it
+				// overlay.detach();
+
+
+
+
+
